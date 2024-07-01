bcb9f7091d19dbc9a7bf35cc8833c2b206eb83f6
Renk Design <51901367+Renk-design@users.noreply.github.com>
2024-07-01 12:57:26 +0300

    Update sd.config.js

--- sd.config.js
+++ sd.config.js
@@ -32,10 +32,30 @@ export default {
 
   source: ['tokens/**/*.json'],
   platforms: {
+    json: {
+      buildPath: 'build/',
+      files: [
+        {
+          destination: 'tokens.json',
+          format: 'json/nested',
+        },
+      ],
+    },
+    js: {
+      buildPath: 'build/',
+      transformGroup: 'js',
+      files: [
+        {
+          destination: 'tokens.js',
+          format: 'customFormat',
+          options: {
+            outputReferences: true,
+          },
+        },
+      ],
+    },
     css: {
       transformGroup: 'css',
-      transforms: ['name/kebab','shadow/css/shorthand'],
-      prefix: 'gf',
       buildPath: 'build/',
       files: [
         {
