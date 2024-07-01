ebbce135c4f630fff84b4fcdf3d575764ceef31a
Renk Design <51901367+Renk-design@users.noreply.github.com>
2024-07-01 11:14:58 +0300

    Update sd.config.js
    add ts

--- sd.config.js
+++ sd.config.js
@@ -1,56 +1,27 @@
-export default {
-  hooks: {
-    formats: {
-      // Adding a custom format to show how to get an alias's name.
-      customFormat: function ({ dictionary, options }) {
-        return dictionary.allTokens
-          .map((token) => {
-            let value = JSON.stringify(token.value);
-            // new option added to decide whether or not to output references
-            if (options.outputReferences) {
-              // the `dictionary` object now has `usesReference()` and
-              // `getReferences()` methods. `usesReference()` will return true if
-              // the value has a reference in it. `getReferences()` will return
-              // an array of references to the whole tokens so that you can access
-              // their names or any other attributes.
-              if (dictionary.usesReference(token.original.value)) {
-                const refs = dictionary.getReferences(token.original.value);
-                refs.forEach((ref) => {
-                  value = value.replace(ref.value, function () {
-                    return `${ref.name}`;
-                  });
-                });
-              }
-            }
+import { registerTransforms } from '@tokens-studio/sd-transforms';
+import StyleDictionary from 'style-dictionary';
 
-            return `export const ${token.name} = ${value};`;
-          })
-          .join(`\n`);
-      },
-    },
-  },
+// will register them on StyleDictionary object
+// that is installed as a dependency of this package.
+registerTransforms(StyleDictionary);
 
-  source: ['tokens/**/*.json'],
+const sd = StyleDictionary.extend({
+  source: ['**/*.json'], // <-- make sure to have this match your token files!!!
+  preprocessors: ['tokens-studio'], // <-- since 0.16.0 this must be explicit
   platforms: {
-    css: {
-      transformGroup: 'css',
-      buildPath: 'build/',
+    scss: {
+      transformGroup: 'tokens-studio', // <-- apply the tokens-studio transformGroup to apply all transforms
+      transforms: ['name/kebab'], // <-- add a token name transform for generating token names, default is camel
+      buildPath: 'build/scss/',
       files: [
         {
-          destination: 'tokens.css',
-          format: 'css/variables',
-          options: {
-            outputReferences: true, // new setting, if true will use variable references
-          },
-        },
-        {
-          destination: 'tokens.scss',
+          destination: 'variables.scss',
           format: 'scss/variables',
-          options: {
-            outputReferences: true, // new setting, if true will use variable references
-          },
         },
       ],
     },
   },
-};
+});
+
+sd.cleanAllPlatforms();
+sd.buildAllPlatforms();
