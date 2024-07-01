56f5136b76724b2cecc6ffbd1498a4ce1a8f4b56
Renk Design <51901367+Renk-design@users.noreply.github.com>
2024-07-01 11:27:18 +0300

    Update sd.config.js

--- sd.config.js
+++ sd.config.js
@@ -5,9 +5,39 @@ import StyleDictionary from 'style-dictionary';
 // that is installed as a dependency of this package.
 registerTransforms(StyleDictionary);
 
-const sd = StyleDictionary.extend({
-  source: ['**/*.json'], // <-- make sure to have this match your token files!!!
-  preprocessors: ['tokens-studio'], // <-- since 0.16.0 this must be explicit
+export default {
+  hooks: {
+    formats: {
+      // Adding a custom format to show how to get an alias's name.
+      customFormat: function ({ dictionary, options }) {
+        return dictionary.allTokens
+          .map((token) => {
+            let value = JSON.stringify(token.value);
+            // new option added to decide whether or not to output references
+            if (options.outputReferences) {
+              // the `dictionary` object now has `usesReference()` and
+              // `getReferences()` methods. `usesReference()` will return true if
+              // the value has a reference in it. `getReferences()` will return
+              // an array of references to the whole tokens so that you can access
+              // their names or any other attributes.
+              if (dictionary.usesReference(token.original.value)) {
+                const refs = dictionary.getReferences(token.original.value);
+                refs.forEach((ref) => {
+                  value = value.replace(ref.value, function () {
+                    return `${ref.name}`;
+                  });
+                });
+              }
+            }
+
+            return `export const ${token.name} = ${value};`;
+          })
+          .join(`\n`);
+      },
+    },
+  },
+
+  source: ['tokens/**/*.json'],
   platforms: {
     scss: {
       transformGroup: 'tokens-studio', // <-- apply the tokens-studio transformGroup to apply all transforms
@@ -21,7 +51,4 @@ const sd = StyleDictionary.extend({
       ],
     },
   },
-});
-
-sd.cleanAllPlatforms();
-sd.buildAllPlatforms();
+};
